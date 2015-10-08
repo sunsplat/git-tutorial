6963c4eea443caedc3fbbac39e872f2547bf2ff1
sunsplat <heeycutie@yahoo.com>
2015-10-08 11:29:41 +0800

    added one more array item to all arrays

--- movie_plot.php
+++ movie_plot.php
@@ -4,7 +4,8 @@ class MoviePlotGenerator {
 									  'diminuitive',
 									  'fat',
 									  'determined',
-									  'girsly');
+									  'girsly',
+									  'extraordinary');
 						
 	public static $occupations = array('movie-critic',
 									   'goat wrangler',
@@ -14,7 +15,8 @@ class MoviePlotGenerator {
 									   'archaeologist',
 									   'investment banker',
 									   'baker',
-									   'swimsuit model');
+									   'swimsuit model',
+									   'astronaut');
 						 
 	public static $quirks = array('penchant for melodrama',
 								  'murky past',
@@ -24,7 +26,8 @@ class MoviePlotGenerator {
 								  'background in Kung Fu',
 								  'baby on the way',
 								  '57\' Chevy',
-								  'family back in Mexico');
+								  'family back in Mexico',
+								  'obsessed with gnomes');
 					
 	/**
 	 *
